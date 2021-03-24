db6d99f1d3d6c28d0a84f2678af59a0d438a0594
Saadia El fekak <saadiaelfekak@gmail.com>
2021-03-24 23:01:54 +0000

    Correcting author id

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,21 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.author_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { username: req.body.username } });
@@ -22,7 +37,7 @@ router.post('/login', async (req, res) => {
     }
 
     req.session.save(() => {
-      req.session.user_id = userData.id;
+      req.session.author_id = userData.id;
       req.session.logged_in = true;
 
       res.json({ user: userData, message: 'You are now logged in!' });
@@ -42,7 +57,7 @@ router.post('/signup', async (req, res) => {
     });
 
     req.session.save(() => {
-      req.session.user_id = userData.id;
+      req.session.author_id = userData.id;
       req.session.logged_in = true;
 
       res.json({ user: userData, message: 'You are now logged in!' });
