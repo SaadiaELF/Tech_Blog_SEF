4eaacd1a0548d448cb39ef0c56f5f93274803bee
Saadia El fekak <saadiaelfekak@gmail.com>
2021-03-25 14:14:54 +0000

    Adding delete post route

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -19,6 +19,26 @@ router.post('/', withAuth, async (req, res) => {
 
 });
 
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.destroy({
+      where: {
+        id: req.params.id,
+        author_id: req.session.author_id,
+      },
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err);
+  }
+});
 
 
 module.exports = router;
