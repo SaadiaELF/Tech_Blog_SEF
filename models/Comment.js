3732ae8f902b4552d2434119f88cd4f40d4cd0c0
Saadia El fekak <saadiaelfekak@gmail.com>
2021-03-25 22:40:50 +0000

    Adding comment relationships

--- models/Comment.js
+++ models/Comment.js
@@ -11,14 +11,14 @@ Comment.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    content: {
+    comment_text: {
       type: DataTypes.STRING(100),
       allowNull: false,
       validate: {
         len: [10],
       },
     },
-    date: {
+    time: {
       type: DataTypes.DATE,
       allowNull: false,
       defaultValue: DataTypes.NOW,
@@ -27,6 +27,13 @@ Comment.init(
       type: DataTypes.INTEGER,
       references: {
         model: 'user',
+        key: 'id',
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'post',
         key: 'id',
       },
     },
