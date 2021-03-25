5546a554b373e180258024a96c984cecd178b1f5
Saadia El fekak <saadiaelfekak@gmail.com>
2021-03-25 15:51:45 +0000

    Adding fuction to delete post

--- public/js/delete-post.js
+++ public/js/delete-post.js
@@ -1,46 +1,19 @@
-const newPostHandler = async (event) => {
-    event.preventDefault();
-  
-    const title = document.querySelector('#title').value.trim();
-    const content = document.querySelector('#content').value.trim();
-  
-    if (title && content ) {
-      const response = await fetch('/api/posts', {
-        method: 'POST',
-        body: JSON.stringify({ title, content }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      } else {
-        alert('Failed to create a post');
-      }
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to delete post');
     }
-  };
+  }
+};
 
-  const delButtonHandler = async (event) => {
-    if (event.target.hasAttribute('data-id')) {
-      const id = event.target.getAttribute('data-id');
-  
-      const response = await fetch(`/api/posts/${id}`, {
-        method: 'DELETE',
-      });
-  
-      if (response.ok) {
-        document.location.replace('/dashboard');
-      } else {
-        alert('Failed to delete post');
-      }
-    }
-  };
-
-
-  // document
-  //   .querySelector('.new-post-form')
-  //   .addEventListener('submit', newPostHandler);
-  
-      
-  document
+document
   .querySelector('.post-list')
   .addEventListener('click', delButtonHandler);
