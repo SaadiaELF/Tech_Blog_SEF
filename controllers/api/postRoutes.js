04c6175b40ef60286579d8cffddfd2c3b2a7098e
Saadia El fekak <saadiaelfekak@gmail.com>
2021-03-25 15:50:17 +0000

    Adding a route to update post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
-// const moment = require('moment');
 
 router.post('/', withAuth, async (req, res) => {
   // create a new post
@@ -35,10 +34,32 @@ router.delete('/:id', withAuth, async (req, res) => {
 
     res.status(200).json(postData);
   } catch (err) {
-    console.log(err)
     res.status(500).json(err);
   }
 });
 
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.update({
+      title: req.body.title,
+      content: req.body.content
+    },
+      {
+        where: {
+          id: req.params.id,
+        },
+      });
+
+    if (!postData) {
+      res.status(404).json({ message: 'No post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(postData);
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
