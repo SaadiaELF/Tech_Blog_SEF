d59ddf5c50320f2d187311857afd485d3f3f41df
Saadia El fekak <saadiaelfekak@gmail.com>
2021-03-26 00:01:24 +0000

    Adding comment model to get routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,36 +1,62 @@
 const router = require('express').Router();
-const { User, Post } = require('../models');
+const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
     const postData = await Post.findAll({
+      attributes: [
+        'id',
+        'title',
+        'content',
+        'date'
+      ],
       include: [
         {
           model: User,
           attributes: ['username'],
         },
-      ],
+        {
+          model: Comment,
+          attributes: ['comment_text','time'],
+          include: {
+            model: User,
+            attributes: ['username']
+          },
+        }],
     });
 
     const posts = postData.map((post) => post.get({ plain: true }));
 
     res.render('homepage', { posts, logged_in: req.session.logged_in });
-
+    console.log(posts)
   } catch (error) {
-    res.status(500).json(err);
+    console.log(error);
+    res.status(500).json(error);
   }
 });
 
 router.get('/post/:id', withAuth, async (req, res) => {
   try {
     const postData = await Post.findByPk(req.params.id, {
+      attributes: [
+        'id',
+        'title',
+        'content',
+        'date'
+      ],
       include: [
         {
           model: User,
           attributes: ['username'],
         },
-      ],
+        {
+          model: Comment,
+          include: {
+            model: User,
+            attributes: ['username']
+          },
+        }],
     });
 
     const post = postData.get({ plain: true });
