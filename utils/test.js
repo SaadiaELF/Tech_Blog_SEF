8f8b91e790efacf6fb23303c3cfd6e9229c2b184
Saadia El fekak <saadiaelfekak@gmail.com>
2021-03-26 17:17:02 +0000

    delete test file

--- utils/test.js
+++
@@ -1,7 +0,0 @@
-if_eq: (a, b) => {
-    if (a == b) {
-      return ` <div class="card-footer text-muted">
-      <button type="button" class="btn btn-danger float-right" style="width: 150px;" data-id="{{post.id}}">DELETE</button>
-      </div>`
-    }
-  }
